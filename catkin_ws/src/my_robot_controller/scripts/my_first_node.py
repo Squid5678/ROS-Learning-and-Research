9d86f4b0f9c996e57b5e1b513e2a10bf3ef6d6af
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-04-04 21:23:11 -0400

    push of last node dev

--- catkin_ws/src/my_robot_controller/scripts/my_first_node.py
+++ catkin_ws/src/my_robot_controller/scripts/my_first_node.py
@@ -1,14 +1,22 @@
-#!/usr/bin/enc python3
+#!/usr/bin/env python3
 import rospy
 
 if __name__ == '__main__':
     rospy.init_node("test_node")
 
-    #this is a print and also saves to logs.
-    rospy.loginfo("Hello from test node")
-    rospy.logwarn("this is a warning :O")
-    rospy.logerr("this is an error")
+    rospy.loginfo("test node has been started")
 
-    rospy.sleep(1) #this is the equivalent of time.sleep
-    rospy.loginfo("end of program!")
+    rate = rospy.Rate(10) #rate of 10 Hz (0.1 seconds)
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("hello")
+        rate.sleep()
+
+    # #this is a print and also saves to logs.
+    # rospy.loginfo("Hello from test node")
+    # rospy.logwarn("this is a warning :O")
+    # rospy.logerr("this is an error")
+
+    # rospy.sleep(1) #this is the equivalent of time.sleep
+    # rospy.loginfo("end of program!")
     
